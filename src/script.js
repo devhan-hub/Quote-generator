b89d633ebf7566c6f8d916489298735a14693645
devhan-hub <hanabduman@gmail.com>
2024-08-27 18:36:04 +0300

    Implement delete functionality for removing quotes from the selected category

--- src/script.js
+++ src/script.js
@@ -7,6 +7,7 @@ const quoteBox = document.querySelector(".qote-box");
 const addQuote = document.getElementById("add");
 const deletQuotevar = document.getElementById("delet");
 
+// retriving from local storage
 let allQuoteObject = JSON.parse(localStorage.getItem("allQuote"));
 
 
@@ -14,24 +15,27 @@ let allQuoteObject = JSON.parse(localStorage.getItem("allQuote"));
 let selectedQuotes = [];
 let selectedCatagory = {} ;
 
-//  default display
-const defaultCategory = "success";
-selectedCatagory = allQuoteObject.find(catagory => catagory.id === defaultCategory);
-selectedQuotes = selectedCatagory.quotes;
-displayQuote();
+// //  default display
+// const defaultCategory = "success";
+// selectedCatagory = allQuoteObject.find(catagory => catagory.id === defaultCategory);
+// selectedQuotes = selectedCatagory.quotes;
+// displayQuote();
 
 
-//  for label style
+
+
+
+const initialSelectedRadio = document.querySelector('input[name="catagory"]:checked');
+applySelectedStyle(initialSelectedRadio);
+
+
+//  for label 
 function resetLabels() {
   labels.forEach(label => {
     label.classList.remove('bg-[#ed9d09]', 'text-white');
     label.classList.add('text-[#ed9d09]', 'bg-transparent');
   });
 }
-
-const initialSelectedRadio = document.querySelector('input[name="catagory"]:checked');
-applySelectedStyle(initialSelectedRadio);
-
 function applySelectedStyle(selectedRadio) {
   const selectedLabel = document.querySelector(`label[for="${selectedRadio.id}"]`);
   selectedLabel.classList.add('bg-[#ed9d09]', 'text-white');
@@ -39,6 +43,15 @@ function applySelectedStyle(selectedRadio) {
 };
 
 
+
+  
+// default Quote
+const defaultCategory = "success";
+selectedCatagory = allQuoteObject.find(catagory => catagory.id === defaultCategory);
+selectedQuotes = selectedCatagory.quotes;
+displayQuote();
+
+// getting selected quote
 quoteCatagory.addEventListener('change', (event) => {
  const selectedCatagoryID = event.target.value;
   const selectedRadio = event.target;
@@ -56,6 +69,7 @@ quoteCatagory.addEventListener('change', (event) => {
   displayQuote();
 });
 
+
 function displayQuote() {
   const selectedQuoteIndex = Math.floor(Math.random() * selectedQuotes.length);
   blockquote.innerHTML = selectedQuotes[selectedQuoteIndex].quote;
@@ -70,16 +84,19 @@ function displayQuote() {
 newButton.addEventListener('click', displayQuote);
 quoteBox.addEventListener('click', displayQuote);
 
-// add logic 
+// add user Quote
 
 addQuote.addEventListener("click", () => {
   const addquoteForm = document.getElementById("userNewQuote");
-  addquoteForm.classList.remove('hidden');
+  addquoteForm.classList.toggle('hidden');
 });
 
 document.getElementById("submit").addEventListener(('click'), () => {
   event.preventDefault();
+
   const addquoteForm =document.getElementById("userNewQuote");
+  const addedQuote =document.getElementById("added_quote").value.trim();
+  const addedquoteauthor= document.getElementById("added_quote_author").value.trim();
 
   if (selectedCatagory.id === "All") {
     alert("Please select a specific category to add the quote.");
@@ -89,22 +106,20 @@ document.getElementById("submit").addEventListener(('click'), () => {
   else {
     let useraddQuote = {
       id: (selectedQuotes.length + 1).toString(),
-      quote: document.getElementById("added_quote").value,
-      author: document.getElementById("added_quote_author").value
+      quote:addedQuote,
+      author:addedquoteauthor
     }
     selectedQuotes.push(useraddQuote);
-    console.log(selectedQuotes);
   
-    
   }
   updatedCatagory();
-  document.getElementById("added_quote").value = "",
-  document.getElementById("added_quote_author").value =" ",
+  addedQuote = "",
+  addedquoteauthor=" ",
   addquoteForm.classList.add('hidden');
 
 });
 
-// delet 
+// delete 
 
 function deletQuote(selectedQuoteIndex) {
   selectedQuotes.splice(selectedQuoteIndex, 1);
@@ -114,15 +129,11 @@ function deletQuote(selectedQuoteIndex) {
 
 function updatedCatagory() {
    let catagoryToUpdate = allQuoteObject.find(catagory => catagory.id === selectedCatagory.id);
-  
-  
- 
    if(catagoryToUpdate) {
     catagoryToUpdate.quotes = selectedQuotes;
     allQuoteString = JSON.stringify(allQuoteObject);
     localStorage.setItem("allQuote", allQuoteString);
    }
-
   
 }
 
