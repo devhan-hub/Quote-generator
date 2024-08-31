91957d69fe39d5cd38ad5d7a86c7426947f67c0c
devhan-hub <hanabduman@gmail.com>
2024-08-31 17:39:12 +0300

    Changing the UI design

--- src/script.js
+++ src/script.js
@@ -20,8 +20,8 @@ const ok = document.getElementById("Ok");
 // retriving from local storage
 
 let allQuoteObject = JSON.parse(localStorage.getItem("allQuote"));
-let favoriteQuotes =allQuoteObject.find(catagory => catagory.id === "favorite");
-favoriteQuotes = favoriteQuotes.quotes ;
+let favoriteQuotes = allQuoteObject.find(catagory => catagory.id === "favorite");
+favoriteQuotes = favoriteQuotes.quotes;
 let selectedQuotes = [];
 let selectedCatagory = {};
 let selectedQuoteIndex;
@@ -32,32 +32,33 @@ applySelectedStyle(initialSelectedRadio);
 //  for label 
 function resetLabels() {
   labels.forEach(label => {
-    label.classList.remove('bg-[#ff6f61]', 'text-white','md:text-[#ed9d09]');
-    label.classList.add('text-black', 'bg-white' , 'md:text-white');
+    label.classList.remove('bg-[#eda218]', 'text-white', 'md:text-[#eda218]');
+    label.classList.add('text-black', 'bg-white', );
   });
 }
 function applySelectedStyle(selectedRadio) {
   const selectedLabel = document.querySelector(`label[for="${selectedRadio.id}"]`);
-  selectedLabel.classList.add('bg-[#ff6f61]', 'text-white' ,'md:text-[#ed9d09]');
-  selectedLabel.classList.remove('text-black', 'bg-white', 'md:text-white');
+  selectedLabel.classList.add('bg-[#eda218]', 'text-white', 'md:text-[#eda218]');
+  selectedLabel.classList.remove('text-black', 'bg-white', );
 };
 
 const defaultCategory = "success";
 selectedCatagory = allQuoteObject.find(catagory => catagory.id === defaultCategory);
 selectedQuotes = selectedCatagory.quotes;
-quotelength= selectedQuotes.length;
+quotelength = selectedQuotes.length;
 displayQuote();
 console.log(allQuoteObject)
 
 // related to display function
-caticon.addEventListener(("click"),() => {
-   quoteCatagory.classList.toggle("right-[-100%]");
+caticon.addEventListener(("click"), () => {
+  quoteCatagory.classList.toggle("right-[-100%]");
 })
 
-display.addEventListener(("click"),()=>{
+display.addEventListener(("click"), () => {
   document.getElementById("displayallFavorite").classList.toggle("hidden");
   display.classList.toggle("text-[#ff6f61]")
-  
+  document.getElementById("quotedisplaybel").classList.toggle("hidden");
+
 })
 
 // getting selected quote
@@ -67,15 +68,15 @@ quoteCatagory.addEventListener('change', (event) => {
   resetLabels();
   applySelectedStyle(selectedRadio);
 
-  setTimeout(() =>{
+  setTimeout(() => {
     quoteCatagory.classList.toggle("right-[-100%]");
-  },500)
+  }, 500)
 
   selectedCatagory = allQuoteObject.find(catagory => catagory.id === selectedCatagoryID);
   if (selectedCatagory) {
-   
-      selectedQuotes = selectedCatagory.quotes;
-      quotelength = selectedQuotes.length;
+
+    selectedQuotes = selectedCatagory.quotes;
+    quotelength = selectedQuotes.length;
   }
   displayQuote();
 });
@@ -85,30 +86,32 @@ function displayQuote() {
   blockquote.textContent = selectedQuotes[selectedQuoteIndex].quote;
   authorofQuote.textContent = selectedQuotes[selectedQuoteIndex].author;
 
-  if(favoriteQuotes.find(favorite => favorite.quote === blockquote.textContent && favorite.author ===  authorofQuote.textContent))
-     {
-      favoriteIcon.classList.add('text-white' , 'bg-red-600')
-     }
-     else {
-      favoriteIcon.classList.remove('text-white' , 'bg-red-600')
+  if (favoriteQuotes.find(favorite => favorite.quote === blockquote.textContent && favorite.author === authorofQuote.textContent)) {
+    favoriteIcon.classList.add('text-red-600' )
+    favoriteIcon.classList.remove('text-[#eda218]')
 
-     }
-     displayFavority()
-     const bloch = document.querySelector(".blockc");
-     bloch.classList.remove("animate-block");
+  }
+  else {
+    favoriteIcon.classList.remove('text-red-600' )
+    favoriteIcon.classList.add('text-[#eda218]')
 
-     void bloch.offsetWidth;
-     bloch.classList.add("animate-block");
+  }
+  displayFavority()
+  const bloch = document.querySelector(".blockc");
+  bloch.classList.remove("animate-block");
+
+  void bloch.offsetWidth;
+  bloch.classList.add("animate-block");
 }
-  displayQuote();
+displayQuote();
 newButton.addEventListener('click', displayQuote);
 
 // add user Quote
 addQuote.addEventListener("click", () => {
   closeQuoteForm();
-     setforAddDelet(1);
+  setforAddDelet(1);
 
- 
+
 });
 // adding validation
 submit.addEventListener(('click'), () => {
@@ -116,41 +119,41 @@ submit.addEventListener(('click'), () => {
   const addedQuote = document.getElementById("added_quote").value.trim();
   const addedquoteauthor = document.getElementById("added_quote_author").value.trim();
 
- 
+
   let valid = true;
   if (!addedQuote || !addedquoteauthor) {
     valid = false;
-    succError(0 ,"Both the Quote and the Author are required")
+    succError(0, "Both the Quote and the Author are required")
   }
 
   let duplicateQuote = selectedQuotes.some(catagory => catagory.quote === addedQuote && catagory.author === addedquoteauthor);
   if (duplicateQuote) {
-       valid = false; 
-        succError(0 ,"The Quote already existe")
-   };
+    valid = false;
+    succError(0, "The Quote already existe")
+  };
 
   if (selectedCatagory.id === "All") {
-    succError(0 ,"Selecte Specfic Catagory")
+    succError(0, "Selecte Specfic Catagory")
     valid = false;
   }
 
   if (valid) {
 
     let useraddQuote = {
-      id: quotelength +1,
+      id: quotelength + 1,
       quote: addedQuote,
       author: addedquoteauthor
     }
-    quotelength =quotelength +1;
+    quotelength = quotelength + 1;
     selectedQuotes.push(useraddQuote);
-     updatedCatagory();
-   succError(1, "Quote Successfully Deleted")
-   submit.classList.add("hidden");
-   ok.classList.remove("hidden");
+    updatedCatagory();
+    succError(1, "Quote Successfully Added")
+    submit.classList.add("hidden");
+    ok.classList.remove("hidden");
   }
 });
 
-ok.addEventListener(("click"),() => {
+ok.addEventListener(("click"), () => {
   closeQuoteForm();
 })
 deletQuotevar.addEventListener("click", () => {
@@ -164,7 +167,7 @@ deletQuotevar.addEventListener("click", () => {
 
 deletYes.addEventListener(('click'), () => {
   event.preventDefault();
-   deletQuote();
+  deletQuote();
 
 });
 deletNo.addEventListener(('click'), () => {
@@ -175,12 +178,12 @@ deletNo.addEventListener(('click'), () => {
 function deletQuote() {
   selectedQuotes.splice(selectedQuoteIndex, 1);
   updatedCatagory();
-  succError(1,"Quote Successfully Deleted")
+  succError(1, "Quote Successfully Deleted")
   displayQuote();
-  document.getElementById("Yes").classList.add("hidden") ;   
-  document.getElementById("No").classList.add("hidden") ;
-  ok.classList.remove("hidden"); 
-  document.getElementById("deletelabel") .classList.add("hidden");
+  document.getElementById("Yes").classList.add("hidden");
+  document.getElementById("No").classList.add("hidden");
+  ok.classList.remove("hidden");
+  document.getElementById("deletelabel").classList.add("hidden");
 }
 
 function updatedCatagory() {
@@ -191,9 +194,9 @@ function updatedCatagory() {
     localStorage.setItem("allQuote", allQuoteString);
     console.log("Updated allQuoteObject:", allQuoteObject);
   }
-   catagoryToUpdate = allQuoteObject.find(catagory => catagory.id === 'favorite');
-   if (catagoryToUpdate) {
-    catagoryToUpdate.quotes =favoriteQuotes;
+  catagoryToUpdate = allQuoteObject.find(catagory => catagory.id === 'favorite');
+  if (catagoryToUpdate) {
+    catagoryToUpdate.quotes = favoriteQuotes;
     const allQuoteString = JSON.stringify(allQuoteObject);
     localStorage.setItem("allQuote", allQuoteString);
   }
@@ -215,88 +218,98 @@ userAddeFormOut.addEventListener('click', (event) => {
 
 function closeQuoteForm() {
   const addquoteForm = document.getElementById("userNewQuote");
- 
-    addquoteForm.classList.toggle('hidden');
-    document.querySelector(".main").classList.toggle("hidden");
-    document.getElementById("errorBox").classList.add("hidden");
-   document.getElementById("errorMessage").textContent = "";
-   document.getElementById("successBox").classList.add("hidden");
-   document.getElementById("successMessage").textContent = "";
-   document.getElementById("added_quote").value = "";
+
+  addquoteForm.classList.toggle('hidden');
+  document.querySelector(".main").classList.toggle("hidden");
+  document.getElementById("errorBox").classList.add("hidden");
+  document.getElementById("errorMessage").textContent = "";
+  document.getElementById("successBox").classList.add("hidden");
+  document.getElementById("successMessage").textContent = "";
+  document.getElementById("added_quote").value = "";
   document.getElementById("added_quote_author").value = " ";
   ok.classList.add("hidden");
-  
- 
- 
+
+
+
 };
 //
- function succError (num , str) {
-      if(num === 1) {
-        document.getElementById("successBox").classList.remove("hidden");
-        document.getElementById("successMessage").textContent = str;
-        document.getElementById("errorBox").classList.add("hidden");
-        document.getElementById("errorMessage").textContent = "";
-      }
+function succError(num, str) {
+  if (num === 1) {
+    document.getElementById("successBox").classList.remove("hidden");
+    document.getElementById("successMessage").textContent = str;
+    document.getElementById("errorBox").classList.add("hidden");
+    document.getElementById("errorMessage").textContent = "";
+  }
 
-      else {
-        document.getElementById("successBox").classList.add("hidden");
-        document.getElementById("successMessage").textContent = "";
-        document.getElementById("errorBox").classList.remove("hidden");
-        document.getElementById("errorMessage").textContent = str;
-      }
- }
+  else {
+    document.getElementById("successBox").classList.add("hidden");
+    document.getElementById("successMessage").textContent = "";
+    document.getElementById("errorBox").classList.remove("hidden");
+    document.getElementById("errorMessage").textContent = str;
+  }
+}
 
 function setforAddDelet(num) {
-     if(num === 1) {
+  if (num === 1) {
 
-      if(document.querySelector(".input-one").classList.contains("hidden")){
-        document.querySelector(".input-one").classList.remove("hidden");
-        document.querySelector(".input-two").classList.remove("hidden");
-        document.querySelector(".added-one").classList.add("hidden") ;  
-        document.querySelector(".added-two").classList.add("hidden") ; 
-      }
-        
-          document.getElementById("deletelabel").classList.add("hidden") ; 
-          document.getElementById("Yes").classList.add("hidden") ;   
-          document.getElementById("No").classList.add("hidden") ;
-           document.getElementById("submit").classList.remove("hidden");
-           
-     }
+    if (document.querySelector(".input-one").classList.contains("hidden")) {
+      document.querySelector(".input-one").classList.remove("hidden");
+      document.querySelector(".input-two").classList.remove("hidden");
+      document.querySelector(".added-one").classList.add("hidden");
+      document.querySelector(".added-two").classList.add("hidden");
+    }
 
-     if(num ===2) {
-      if(!document.querySelector(".input-one").classList.contains("hidden")){
-        document.querySelector(".input-one").classList.add("hidden");
-        document.querySelector(".input-two").classList.add("hidden");
-        document.querySelector(".added-one").classList.remove("hidden") ;  
-        document.querySelector(".added-two").classList.remove("hidden") ; 
-      }  
-        document.getElementById("submit").classList.add("hidden");
-       
-          document.getElementById("deletelabel").classList.remove("hidden") ; 
-          document.getElementById("Yes").classList.remove("hidden") ;   
-          document.getElementById("No").classList.remove("hidden") ; 
-        
-   }
+    document.getElementById("deletelabel").classList.add("hidden");
+    document.getElementById("Yes").classList.add("hidden");
+    document.getElementById("No").classList.add("hidden");
+    document.getElementById("submit").classList.remove("hidden");
+
+  }
+
+  if (num === 2) {
+    if (!document.querySelector(".input-one").classList.contains("hidden")) {
+      document.querySelector(".input-one").classList.add("hidden");
+      document.querySelector(".input-two").classList.add("hidden");
+      document.querySelector(".added-one").classList.remove("hidden");
+      document.querySelector(".added-two").classList.remove("hidden");
+    }
+    document.getElementById("submit").classList.add("hidden");
+
+    document.getElementById("deletelabel").classList.remove("hidden");
+    document.getElementById("Yes").classList.remove("hidden");
+    document.getElementById("No").classList.remove("hidden");
+
+  }
 }
 function displayFavority() {
-     favoriteQuoteList.innerHTML = '',
-     favoriteQuotes.forEach(quot => {
+  favoriteQuoteList.innerHTML = '',
+    favoriteQuotes.forEach(quot => {
       let li = document.createElement("li");
-      li.textContent = `${quot.quote}, \"${quot.type}\"`;
-
-     li.className = 'gap-5 flex px-2 py-4 shadow-3xl bg-slate-500 text-white text-lg font-quote';
+      let div = document.createElement("div");
+      let p = document.createElement("p");
+      let p2 = document.createElement('p');
+      p2.textContent = quot.quote;
+      p.textContent = quot.type;
+      div.className = 'flex flex-col items-start justify-center gap-2'
+      p.className = 'lg:text-lg  author text-[#f4c060] italic text-[15px]'
+      li.className = 'gap-5 flex px-4 items-center justify-between px-2 py-4 shadow-3xl  text-gray-800 text-lg font-quote bg-white';
       const removeBtn = document.createElement("button");
-      removeBtn.className ='self-center rounded-2xl duration-300 hover:bg-red-700 ease-in-out px-3 py-2 bg-red-500 h-max w-max text-sm';
-      removeBtn.textContent='Remove';
-      removeBtn.onclick =() => removeFavorite(quot);
+      removeBtn.className = 'self-center rounded-2xl duration-300 hover:bg-red-700 ease-in-out px-3 py-2 bg-red-500 h-max w-max text-white text-sm';
+      removeBtn.textContent = 'Remove';
+      removeBtn.onclick = () => removeFavorite(quot);
+      div.appendChild(p2);
+      div.appendChild(p)
+      li.appendChild(div);
       li.appendChild(removeBtn);
+
+
       favoriteQuoteList.appendChild(li);
-     })
+    })
 }
 
-favoriteIcon.addEventListener(("click") ,()=> {
-  let AddToFavorite = favoriteQuotes.find(AddToFavorite => AddToFavorite.id === selectedQuotes[selectedQuoteIndex].id && AddToFavorite.type === selectedCatagory.id )
-  if(AddToFavorite){
+favoriteIcon.addEventListener(("click"), () => {
+  let AddToFavorite = favoriteQuotes.find(AddToFavorite => AddToFavorite.id === selectedQuotes[selectedQuoteIndex].id && AddToFavorite.type === selectedCatagory.id || AddToFavorite.type === selectedQuotes[selectedQuoteIndex].type)
+  if (AddToFavorite) {
     removeFavorite(AddToFavorite)
   }
   else {
@@ -306,33 +319,44 @@ favoriteIcon.addEventListener(("click") ,()=> {
 })
 
 function savetoFavorite() {
- let favQuote =  selectedQuotes[selectedQuoteIndex];
- favQuote ={
-        ...selectedQuotes[selectedQuoteIndex],
-          type:selectedCatagory.id
-       }
-        favoriteQuotes.push(favQuote);
-        console.log(favQuote)
-        updatedCatagory()
-        favoriteIcon.classList.add( 'text-white','bg-red-600');  
-        displayFavority()
-   
+  let favQuote = selectedQuotes[selectedQuoteIndex];
+  let typeId;
+  if (selectedCatagory.id === "All" || selectedCatagory.id === "favorite") {
+    typeId = selectedQuotes[selectedQuoteIndex].type;
+  }
+
+  else {
+    typeId = selectedCatagory.id;
+  }
+  favQuote = {
+    ...selectedQuotes[selectedQuoteIndex],
+    type: typeId
+  }
+  favoriteQuotes.push(favQuote);
+  console.log(favQuote)
+  updatedCatagory()
+  favoriteIcon.classList.add('text-red-600' )
+  favoriteIcon.classList.remove('text-[#eda218]')
+  displayFavority()
+
 }
 
-function removeFavorite(quote){
-    favoriteQuotes = favoriteQuotes.filter(fav =>fav !== quote)
-    console.log(favoriteQuotes)
-    updatedCatagory()
+function removeFavorite(quote) {
+  favoriteQuotes = favoriteQuotes.filter(fav => fav !== quote)
+  console.log(favoriteQuotes)
+  updatedCatagory()
 
-    favoriteIcon.classList.remove( 'text-white','bg-red-600');  
-      displayFavority()
+  favoriteIcon.classList.remove('text-red-600' )
+    favoriteIcon.classList.add('text-[#eda218]')
+  displayFavority()
 }
 
 clearFavority.addEventListener(("click"), () => {
-  favoriteQuotes =[];
+  favoriteQuotes = [];
   updatedCatagory();
   displayFavority();
-  favoriteIcon.classList.remove( 'text-white','bg-red-600'); 
+  favoriteIcon.classList.remove('text-red-600' )
+    favoriteIcon.classList.add('text-[#eda218]')
 })
 
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,40 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+
   theme: {
-    extend: {
-      fontFamily:{
-        quote:'"Playfair Display", serif',
-        author:'"Roboto", sans-serif'
-
-      },
-      colors:{
-        commen:'#56aeff',
-      },
-      backgroundImage: {
-        'custom-radial': 'radial-gradient(circle farthest-side at 50% 50%, rgb(239, 205, 142), rgb(241, 199, 119), rgb(225, 176, 85))',
-      },
-      boxShadow:{
-        boxshadow:"2px 2px 2px 2px rgba(0,0,0,0.1)",
-      },
-      screens:{
-        mdet:'550px',
-        
-      },
-      transitionProperty:{
-        dire:"top, left,right",
-      },
-      animation:{
-        'block':'block .5s ease-in-out forwards',
-      },
-      keyframes:{
-           block:{
-            '0%':{left:'-10px', opacity:'0'},
-            '100%':{left:'0px' , opacity:'1'}
-           }
-      },
-
-    },
+    extend: {},
   },
   plugins: [],
 }
